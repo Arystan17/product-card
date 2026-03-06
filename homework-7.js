c8293cf6e64fe8751fbdda48c2f1d00683c7cdf7
Arystan17 <zhusupbaev11@bk.ru>
2026-03-06 06:19:40 +0600

    fixed errors, changed names

--- homework-7.js
+++ homework-7.js
@@ -18,7 +18,7 @@ console.log(checkAvangard);
 // Задание 4 - Написать функцию, которая аргументом будет принимать массив и изменять его порядок на противоположный
 
 function getReversedArray(array) {
-  return [...array].reverse();
+    return [...array].reverse();
 }
 
 console.log(getReversedArray(numbers));
@@ -31,27 +31,27 @@ console.log(comEmails);
 
 // Задание 8 - Перебрать массив таким образом, что бы пользователи с id меньше или равно 5 имели postId: 2, а те, у кого id больше 5, имели postId: 1
 
-const updateComments = comments.map(comment => ({ 
-  ...comment,
-  postId: comment.id <= 5 ? 2 : 1
+const updateComments = comments.map(comment => ({
+    ...comment,
+    postId: comment.id <= 5 ? 2 : 1
 }));
 
 console.log(updateComments);
 
 // Задание 9 - Перебрать массив, чтобы объекты состояли только из айди и имени
 
-const idAndName = comments.map(comment => ({
-  id: comment.id,
-  name: comment.name,
+const commentsIdAndName = comments.map(comment => ({
+    id: comment.id,
+    name: comment.name
 }));
 
-console.log(idAndName);
+console.log(commentsIdAndName);
 
 // Задание 10 - Перебираем массив, добавляем объектам свойство isInvalid
 
 const commentsWithValidity = comments.map(comment => ({
-  ...comment,
-  isInvalid: comment.body.length > 180
+    ...comment,
+    isInvalid: comment.body.length > 180
 }));
 
 console.log(commentsWithValidity);
@@ -59,8 +59,8 @@ console.log(commentsWithValidity);
 // Задание 11 - Почитать про метод массива reduce. Используя его, вывести массив почт и провернуть тоже самое с помощью метода map
 
 const emailsWithReduce = comments.reduce((accumulator, comment) => {
-  accumulator.push(comment.email);
-  return accumulator;
+    accumulator.push(comment.email);
+    return accumulator;
 }, []);
 
 console.log(emailsWithReduce);
